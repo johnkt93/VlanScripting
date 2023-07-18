7e875b6cb1fc519ff12959626a738f3eead59848
John <johnkt@bu.edu>
2023-07-18 12:56:19 -0400

    Fixed global variables for new window. Fixed new window function.

--- NewVlanRouter.py
+++ NewVlanRouter.py
@@ -40,6 +40,86 @@ def file_exists(file_name):
 #def switch_ssh():
 #    connect(device=switch_var.get(),username=username_var.get(), password=password_var.get())
 
+def new_window():
+    new_window = tk.Toplevel(window)
+    new_window.title("VLAN Creation Tool")
+
+    global change_number_var
+    global vlan_number_var
+    global vlan_name_var
+    global vlan_description_var
+    global vlan_gateway_var
+    global vlan_netmask_var
+    global switchport_var
+
+    change_number_var = tk.StringVar()
+    change_number_label = tk.Label(new_window, text="Change Request Number:")
+    change_number_label.pack(pady=5)
+    change_number_input = tk.Entry(new_window, textvariable=change_number_var)
+    change_number_input.pack()
+
+    router_var = tk.StringVar(new_window)
+    router_var.set(routers[0])
+    router_dropdown_label = tk.Label(new_window, text="Source Distribution Router")
+    router_dropdown_label.pack()
+    router_dropdown = tk.OptionMenu(new_window, router_var, *routers)
+    router_dropdown.pack()
+
+    destination_switch_frame = tk.Frame(new_window)
+    destination_switch_frame.pack()
+    switch_var = tk.StringVar()
+    switch_label = tk.Label(destination_switch_frame, text="Destination switch:")
+    switch_label.pack()
+    switch_input = tk.Entry(destination_switch_frame, textvariable=switch_var)
+    switch_input.pack(side=tk.LEFT, padx=5)
+    switch_check_button = tk.Button(destination_switch_frame, text="Check", command=check_switch)
+    switch_check_button.pack(side=tk.RIGHT)
+
+    switchport_var = tk.StringVar(new_window)
+    switchport_label = tk.Label(new_window, text="Enter the switchport that the end device is connected to:")
+    switchport_label.pack()
+    switchport_input = tk.Entry(new_window, textvariable=switchport_var)
+    switchport_input.pack()
+
+    vlan_number_var = tk.StringVar()
+    vlan_number_label = tk.Label(new_window, text="Vlan Number:")
+    vlan_number_label.pack()
+    vlan_number_input = tk.Entry(new_window, textvariable=vlan_number_var)
+    vlan_number_input.pack()
+    vlan_name_var = tk.StringVar()
+    vlan_name_label = tk.Label(new_window, text="Vlan Name:")
+    vlan_name_label.pack()
+    vlan_name_label_input = tk.Entry(new_window, textvariable=vlan_name_var)
+    vlan_name_label_input.pack()
+    vlan_gateway_var = tk.StringVar()
+    vlan_gateway_label = tk.Label(new_window, text="Enter the gateway address for the subnet")
+    vlan_gateway_label.pack()
+    vlan_gateway_input = tk.Entry(new_window, textvariable=vlan_gateway_var)
+    vlan_gateway_input.pack()
+    vlan_netmask_var = tk.StringVar()
+    vlan_netmask_label = tk.Label(text="Enter the subnet mask. Defaults to 255.255.255.0") or "255.255.255.0"
+    vlan_netmask_label.pack()
+    vlan_netmask_input = tk.Entry(new_window, textvariable=vlan_netmask_var)
+    vlan_netmask_input.pack()
+    vlan_description_var = tk.StringVar()
+    vlan_description_label = tk.Label(new_window, text="Enter a brief description for the vlan")
+    vlan_description_label.pack()
+    vlan_description_label_input = tk.Entry(new_window, textvariable=vlan_description_var)
+    vlan_description_label_input.pack()
+
+    multicast_checkbox_var = tk.IntVar()
+    multicast_checkbox = tk.Checkbutton(new_window, text="Check if multicast was requested", variable=multicast_checkbox_var)
+    multicast_checkbox.pack()
+
+    button_frame = tk.Frame(new_window)
+    button_frame.pack(side=tk.BOTTOM, pady=5)
+
+    confirm_button = tk.Button(button_frame, text= "Confirm", command=confirm_command)
+    confirm_button.pack(side=tk.LEFT, padx=10, pady=5)
+
+    cancel_button = tk.Button(button_frame, text = "Cancel", command=new_window.destroy)
+    cancel_button.pack(side=tk.RIGHT, padx=10, pady=5)
+
 username_var = tk.StringVar
 username_label = tk.Label(window, text="Enter your username")
 username_label.pack(pady=10)
@@ -61,7 +141,7 @@ alt_password_input.pack()
 button_frame = tk.Frame(window)
 button_frame.pack(side=tk.BOTTOM, pady=5)
 
-confirm_button = tk.Button(button_frame, text= "Confirm", command=new_window())
+confirm_button = tk.Button(button_frame, text= "Confirm", command=new_window)
 confirm_button.pack(side=tk.LEFT, padx=10, pady=5)
 
 cancel_button = tk.Button(button_frame, text = "Cancel", command=window.destroy)
@@ -113,76 +193,5 @@ end
     else:
         with open(file_name, "w") as file:
             file.write(file_content)
-def new_window():
-    new_window = tk.Toplevel(window)
-    new_window.title("VLAN Creation Tool")
-
-change_number_var = tk.StringVar()
-change_number_label = tk.Label(new_window, text="Change Request Number:")
-change_number_label.pack(pady=5)
-change_number_input = tk.Entry(new_window, textvariable=change_number_var)
-change_number_input.pack()
-
-router_var = tk.StringVar(new_window)
-router_var.set(routers[0])
-router_dropdown_label = tk.Label(new_window, text="Source Distribution Router")
-router_dropdown_label.pack()
-router_dropdown = tk.OptionMenu(new_window, router_var, *routers)
-router_dropdown.pack()
-
-destination_switch_frame = tk.Frame(new_window)
-destination_switch_frame.pack()
-switch_var = tk.StringVar()
-switch_label = tk.Label(destination_switch_frame, text="Destination switch:")
-switch_label.pack()
-switch_input = tk.Entry(destination_switch_frame, textvariable=switch_var)
-switch_input.pack(side=tk.LEFT, padx=5)
-switch_check_button = tk.Button(destination_switch_frame, text="Check", command=check_switch)
-switch_check_button.pack(side=tk.RIGHT)
-
-switchport_var = tk.StringVar(new_window)
-switchport_label = tk.Label(new_window, text="Enter the switchport that the end device is connected to:")
-switchport_label.pack()
-switchport_input = tk.Entry(new_window, textvariable=switchport_var)
-switchport_input.pack()
-
-vlan_number_var = tk.StringVar()
-vlan_number_label = tk.Label(new_window, text="Vlan Number:")
-vlan_number_label.pack()
-vlan_number_input = tk.Entry(new_window, textvariable=vlan_number_var)
-vlan_number_input.pack()
-vlan_name_var = tk.StringVar()
-vlan_name_label = tk.Label(new_window, text="Vlan Name:")
-vlan_name_label.pack()
-vlan_name_label_input = tk.Entry(new_window, textvariable=vlan_name_var)
-vlan_name_label_input.pack()
-vlan_gateway_var = tk.StringVar()
-vlan_gateway_label = tk.Label(new_window, text="Enter the gateway address for the subnet")
-vlan_gateway_label.pack()
-vlan_gateway_input = tk.Entry(new_window, textvariable=vlan_gateway_var)
-vlan_gateway_input.pack()
-vlan_netmask_var = tk.StringVar()
-vlan_netmask_label = tk.Label(text="Enter the subnet mask. Defaults to 255.255.255.0") or "255.255.255.0"
-vlan_netmask_label.pack()
-vlan_netmask_input = tk.Entry(new_window, textvariable=vlan_netmask_var)
-vlan_netmask_input.pack()
-vlan_description_var = tk.StringVar()
-vlan_description_label = tk.Label(new_window, text="Enter a brief description for the vlan")
-vlan_description_label.pack()
-vlan_description_label_input = tk.Entry(new_window, textvariable=vlan_description_var)
-vlan_description_label_input.pack()
-
-multicast_checkbox_var = tk.IntVar()
-multicast_checkbox = tk.Checkbutton(new_window, text="Check if multicast was requested", variable=multicast_checkbox_var)
-multicast_checkbox.pack()
-
-button_frame = tk.Frame(new_window)
-button_frame.pack(side=tk.BOTTOM, pady=5)
-
-confirm_button = tk.Button(button_frame, text= "Confirm", command=confirm_command)
-confirm_button.pack(side=tk.LEFT, padx=10, pady=5)
-
-cancel_button = tk.Button(button_frame, text = "Cancel", command=new_window.destroy)
-cancel_button.pack(side=tk.RIGHT, padx=10, pady=5)
 
 window.mainloop()
